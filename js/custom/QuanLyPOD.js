e04bb3def0fa5edddf447fa334b7115ac423309d
bac.nguyen1 <bac.nguyen1@als.com.vn>
2024-09-23 10:59:07 +0700

    chỉnh  webconfig

--- js/custom/QuanLyPOD.js
+++ js/custom/QuanLyPOD.js
@@ -401,7 +401,7 @@ function fncClick() {
         fncLoadTruckPOD();
     });
 
-    // Xuất báo cáo sản lượng 2  - v1
+    // Xuất báo cáo sản lượng 2  - v2
     $("#btn-xuatbaocao").click(function () {
         var g_tenfile = "GTTvsGTVBaoCao";
         var ajaxGet3 = { "get1": g_tenfile, "get2": "", "get3": 3 };
