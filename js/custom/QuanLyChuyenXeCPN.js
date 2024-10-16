e104c34ea8491ea331de49340c245942dbbbb8b0
DESKTOP-TGGFVSR\bacng <bac.nguyen1@als.com.vn>
2024-10-16 15:09:51 +0700

    update CPN và báo cáo

--- js/custom/QuanLyChuyenXeCPN.js
+++ js/custom/QuanLyChuyenXeCPN.js
@@ -1,1 +1,22 @@
-﻿
+﻿$(document).ready(function () {
+    fncload();
+    fncChange();
+    fncClick();
+    fncModal();
+});
+
+function fncLoad(){
+
+}
+
+function fncChange(){
+
+}
+
+function fncClick(){
+
+}
+
+function fncModal(){
+
+}
