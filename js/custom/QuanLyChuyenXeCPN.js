d1ad2e2e1ea9f03c8cbac1960b7fa8f729d425a3
DESKTOP-TGGFVSR\bacng <bac.nguyen1@als.com.vn>
2024-10-16 15:57:43 +0700

    update convert float to deimal

--- js/custom/QuanLyChuyenXeCPN.js
+++ js/custom/QuanLyChuyenXeCPN.js
@@ -6,17 +6,17 @@
 });
 
 function fncLoad(){
-
+    // Đây là load
 }
 
 function fncChange(){
-
+    // Đây là change
 }
 
 function fncClick(){
-
+    // Đây là click
 }
 
 function fncModal(){
-
+    // Đây là modal
 }
